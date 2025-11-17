5ba17a3525333f7542f5f5f3acffff6110a14f49
Demetar Kalev <kalevdemetar@gmail.com>
2025-11-17 09:30:34 -0500

    Set Up Prettier

--- index.ts
+++ index.ts
@@ -1,19 +1,17 @@
 import concurrently from 'concurrently';
 import { server } from 'typescript';
 
-
 concurrently([
-
-    {
-        name: 'server',
-        command: 'bun run dev',
-        cwd: 'packages/server',
-        prefixColor: 'cyan'
-    },
-    {
-        name: 'client',
-        command: 'bun run dev',
-        cwd: 'packages/client',
-        prefixColor: 'green'
-    },
-])
+   {
+      name: 'server',
+      command: 'bun run dev',
+      cwd: 'packages/server',
+      prefixColor: 'cyan',
+   },
+   {
+      name: 'client',
+      command: 'bun run dev',
+      cwd: 'packages/client',
+      prefixColor: 'green',
+   },
+]);
